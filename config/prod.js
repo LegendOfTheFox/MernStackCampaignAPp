db2bb1ad5598389bdae16c1a8d4ca42753b07950
Bryan PC <bryanfox1986@gmail.com>
2018-10-05 23:07:34 -0400

    auth setup

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+// 198057114116-oflingqfc9dp6i7tgtb9l0gv4fjahicr.apps.googleusercontent.com
+// L8ee9vfdAlMegIEifF-6lF-h
+// mongodb://<dbuser>:<dbpassword>@ds123783.mlab.com:23783/mernstack-prod
