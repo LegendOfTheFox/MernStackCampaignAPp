d459a9040847b0c456f2f8bfbc151c39bc55709a
Bryan PC <bryanfox1986@gmail.com>
2018-10-17 23:17:12 -0400

    started billing implementation

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
-// 198057114116-oflingqfc9dp6i7tgtb9l0gv4fjahicr.apps.googleusercontent.com
-// L8ee9vfdAlMegIEifF-6lF-h
-// mongodb://<dbuser>:<dbpassword>@ds123783.mlab.com:23783/mernstack-prod
