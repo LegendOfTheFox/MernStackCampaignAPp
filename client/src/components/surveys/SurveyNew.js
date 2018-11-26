2043bae4b7621c8a8bf4da9f6bd95a3e617f6f8d
Bryan PC <bryanfox1986@gmail.com>
2018-11-26 00:03:18 -0500

    form validation and reduxForm func added

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          <SurveyForm />
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
