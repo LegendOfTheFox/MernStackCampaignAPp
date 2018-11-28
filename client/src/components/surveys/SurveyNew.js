b45a75fe8c92e328a757a9930d3fdc0e98293fa2
Bryan PC <bryanfox1986@gmail.com>
2018-11-27 23:47:42 -0500

    almost done multipage form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+  state = { showFormReview: false }; //shortcut to state constructor
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
-        <h2>
-          <SurveyForm />
-        </h2>
+        <h2>{this.renderContent()}</h2>
       </div>
     );
   }
